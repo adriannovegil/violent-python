6118f85fdc663389d62c930a969bbb552e1ed69a
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-03-22 23:12:28 +0100

    Modificación del algoritmo de fuerza bruta para romper passwords. Sporte para sha512.

---
+++ ch01/02/sha512/password-cracker.py
@@ -0,0 +1,48 @@
+import crypt
+
+# Funcion que se encarga de romper el password. Como parametros le pasamos
+# uno de los password que hemos extraido del fichero de passwords. La funcion
+# se encarga de recorrer el fichero de diccionario, cifrar cada uno de los password
+# y compararlo con el original para ver si hay coincidencia.
+def testPass(cryptPass):
+	ctype = cryptPass.split("$")[1]
+	#salt = cryptPass[0:2]
+   	if ctype == '6': # $6$ is for SHA 512
+   		print "[+] Hash type SHA-512 detected ..."
+   		print "[+] Be patien ..."
+   		salt = cryptPass.split("$")[2]   	
+   		print "[+] Salt: " + salt	
+		insalt = "$" + ctype + "$" + salt + "$"
+		print "[+] Insalt: " + insalt	
+		# Fichero de diccionario.
+		dictFile = open('dictionary.txt','r')
+		# Recorresmos las palabras del fichero de diccionario. Cifraremos y compararamos
+		# cada una de las palabras.
+		for word in dictFile.readlines():
+			word = word.strip('\n')
+			# Ciframos la palabra.
+			cryptWord = crypt.crypt(word,insalt)
+			print "[+] Hash value: " + cryptWord 
+			# Comparamos el password original con la palabra cifrada para ver si hay
+			# coincidencia.
+			if (cryptWord == cryptPass):
+				print "[+] Found Password: "+ word + "\n"
+				return
+		print "[-] Password Not Found.\n"
+	return
+
+# Funcion principal.
+def main():
+	# Fichero que contiene los password que queremos romper.
+	passFile = open('shadow')
+	# Leemos las lineas del fichero de passwords.
+	for line in passFile.readlines():
+		if 	":" in line:
+			user = line.split(':')[0]
+			cryptPass = line.split(':')[1].strip(' ')
+			print "[*] Cracking Password For: " + user
+			testPass(cryptPass)
+
+# Arrancamos!!!
+if __name__ == "__main__":
+	main()
