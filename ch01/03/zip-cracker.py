b4e943a597f0b12e44d4434e6dbff2b7bebdc20f
Adrian Novegil Toledo <adrian.novegil@gmail.com>
2015-03-23 20:37:05 +0100

    Se ha modularizado el código del programa en funciones independientes.

--- ch01/03/zip-cracker.py
+++ ch01/03/zip-cracker.py
@@ -1,12 +1,24 @@
 import 	zipfile
 
-zFile = zipfile.ZipFile("evil.zip")
-passFile = open('dictionary.txt')
-for line in passFile.readlines():
-	password = line.strip('\n')
+# Funcion que valida si el password es necesario para el fichero.
+def extractFile(zFile, password):
 	try:
 		zFile.extractall(pwd=password)
-		print '[+] Password = ' + password + '\n'
-		exit(0)
-	except Exception, e:
-		pass
+		return password;
+	except:
+		return
+
+# Funcion principal. Recorre el fichero de diccionario y prueba claves
+# para ver si son la adecuada.
+def main():
+	zFile = zipfile.ZipFile("evil.zip")
+	passFile = open('dictionary.txt')
+	for line in passFile.readlines():
+		password = line.strip('\n')
+		guess = extractFile(zFile, password)
+		if guess:
+			print '[+] Password = ' + password + '\n'
+			exit(0)
+
+if __name__ == '__main__':
+	main()	
